720c86da6b57fb6bf38a5eaea7cf0efcfaffdef6
shijunlee <lishjun10@hotmail.com>
2021-08-26 18:55:11 +0800

    add db model

--- go.mod
+++ go.mod
@@ -17,7 +17,7 @@ require (
 	github.com/spf13/viper v1.8.1
 	go.uber.org/zap v1.18.1
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
-	k8s.io/api v0.21.3
+	gorm.io/gorm v1.21.14
 	k8s.io/apimachinery v0.22.1
 	k8s.io/client-go v0.21.3
 	sigs.k8s.io/controller-runtime v0.9.6
--- pkg/dockerauth/policy/database/db.go
+++ pkg/dockerauth/policy/database/db.go
@@ -1,3 +1,15 @@
 package policy
 
+import (
+	"gorm.io/gorm"
+)
+
 //TODO use gorm support this for mysql sqlite pg etc/
+type Policy struct {
+	gorm.Model
+	Username  string   `json:"string"`
+	RepoName  string   `json:"repoName"`
+	Operation string   `json:"operation"`
+	Actions   []string `json:"actions"`
+	Type      string   `json:"type"`
+}
